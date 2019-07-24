f85400af309c6044f32569383e48680e4efdba0a
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-24 10:50:53 -0700

    created Form.js form works but returns username undefined

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width="400px"/>
-
+          Pokemon Trainer Team Builder
       </header>
       <Form />
     </div>
